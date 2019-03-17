7e966abb20fa1e55dbab29674b8c7be3e88124a9
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-03-17 16:58:08 -0300

    form focus event added

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      focused: false
+    };
+  }
+
   handleChange = (event) => {
-    // this.fetchGif(event.target.value);
     this.props.searchFunction(event.target.value);
+  }
+
+  handleFocus = () => {
+    this.setState({
+      focused: true
+    });
+    console.log(this.state.focused);
+  }
+
+  handleBlur = () => {
+    this.setState({
+      focused: false
+    });
+    console.log(this.state.focused);
   }
 
   render () {
     return (
-      <input type="text" className="form-control form-search" onChange={this.handleChange}/>
+      <input type="text" className={`form-control form-search${this.state.focused ? " focus" : ""}`} onChange={this.handleChange} onFocus={this.handleFocus} onBlur={this.handleBlur} />
     );
   }
 }
